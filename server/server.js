21ff332abbf5e12dcf353762707268d9daa8afa5
Jesse Boakye-Donkor <jes142dd@ufl.edu>
2020-03-17 07:39:53 -0400

    changed path for serving static files

--- server/server.js
+++ server/server.js
@@ -17,10 +17,10 @@ app.use('/api/users',users)
 app.use('/api/post',post)
 if (process.env.NODE_ENV === 'production') {
     // Set static folder
-    app.use(express.static('client/build'));
+    app.use(express.static('../client/build'));
   
     app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client','stf','build', 'index.html'));
+      res.sendFile(path.resolve(__dirname,'..' ,'client','build', 'index.html'));
     });
   }
 
