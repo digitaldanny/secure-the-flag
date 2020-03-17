09fd2aced26654e24365e342737a5a2c6d73178d
Giovanni Gaspard <giovannigaspard@Giovannis-MacBook-Pro.local>
2020-03-17 00:28:39 -0400

    edits

--- client/stf/src/config/keys.js
+++ client/stf/src/config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     signupURL: `${process.env.REACT_APP_BASE_API_URL}/api/users/`|| "http://localhost:5000/api/users" ,
     signinURL: `${process.env.REACT_APP_BASE_API_URL}/api/users/`|| "http://localhost:5000/api/users",
-    postURL: "http://localhost:5000/api/post/" 
+    postURL: `${process.env.REACT_APP_BASE_API_URL}/api/post/`||  "http://localhost:5000/api/post/" 
 }
